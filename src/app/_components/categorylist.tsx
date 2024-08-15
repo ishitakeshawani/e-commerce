69ccfe00cdd60e27341c28d1dba71ea8335df093
ishitakeshawani <keshwaniishita054@gmail.com>
2024-08-15 17:13:10 +0530

    Created category list page

---
+++ src/app/_components/categorylist.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+
+interface Category {
+  id: number;
+  name: string;
+  description: string;
+}
+
+interface CategoryListProps {
+  categories: Category[];
+  selectedCategories: number[];
+  onSelect: (id: number) => void;
+}
+
+const CategoryList: FC<CategoryListProps> = ({ categories, selectedCategories, onSelect }) => (
+  <ul className="my-2">
+    {categories.map((category) => (
+      <li key={category.id} className="py-1 flex items-center">
+        <input
+          type="checkbox"
+          checked={selectedCategories.includes(category.id)}
+          onChange={() => onSelect(category.id)}
+          className="mr-2 cursor-pointer"
+        />
+        {category.name}
+      </li>
+    ))}
+  </ul>
+);
+
+export default CategoryList;
